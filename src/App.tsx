e011165a60baab3b876fa6aff1fd8dfcdad956b7
Vlad Harkusha <sivari888@gmail.com>
2022-07-21 17:03:32 +0300

    getting users from server

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Card, CardVariant } from "./components/Card";
 import { UserList } from "./components/UserList";
 import { IUser } from "./types/types";
 
 export const App = () => {
-  const users: IUser[] = [
-    {id: 1, name: 'Vladys', email: 'fiii@gmail.com', address: {city: 'Sumy', street: 'Zalyvna', zipcode: '56687'}},
-    {id: 2, name: 'Kulo', email: 'hsdfgee@gmail.com', address: {city: 'Pskov', street: 'Desantnikov', zipcode: '66687'}},
-  ];
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  async function fetchUsers() {
+    try {
+      const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users');
+
+      setUsers(response.data);
+    } catch (error) {
+      alert(error)
+    }
+  }
 
   return (
     <div>
